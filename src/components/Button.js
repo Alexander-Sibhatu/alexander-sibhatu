c4337e8bc8bc36cdd3ed4d083dc1343eedce0988
Alexander Sibhatu <sibhatualexander@gmail.com>
2024-03-27 20:32:55 +0000

    first check me button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = () => {
   return (
-    <div className='rounded-md bg-[#1ABC9C] '>Button</div>
+    <div className='border-2 hover:border-[#1ABC9C] rounded-md bg-[#1ABC9C] px-4 py-1 my-4 font-4xl font-semibold text-[#FFF8E7] hover:text-[#1ABC9C] hover:bg-[#FFF8E7] drop-shadow-lg cursor-pointer'>CHECK MY RESUME</div>
   )
 }
 

075603a18036bad6cb0f38712c3809690ec54414
Alexander Sibhatu <sibhatualexander@gmail.com>
2024-04-02 23:27:31 +0100

    data

---
+++ src/data/data.js
@@ -0,0 +1,70 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export const Techskills = [
+    {
+        id: uuidv4(),
+        img: '',
+        alt: 'logo',
+        desc: 'React'
+    },
+    {
+        id: uuidv4(),
+        img: '',
+        alt: 'logo',
+        desc: 'Node.js | Express.js'
+    },
+    {
+        id: uuidv4(),
+        img: '',
+        alt: 'logo',
+        desc: 'TypeScript'
+    },
+    {
+        id: uuidv4(),
+        img: '',
+        alt: 'logo',
+        desc: 'Tailwind CSS'
+    },
+    {
+        id: uuidv4(),
+        img: '',
+        alt: 'logo',
+        desc: 'Material-UI'
+    },
+    {
+        id: uuidv4(),
+        img: '',
+        alt: 'logo',
+        desc: 'Ant Design'
+    },
+    {
+        id: uuidv4(),
+        img: '',
+        alt: 'logo',
+        desc: 'Jira'
+    },
+    {
+        id: uuidv4(),
+        img: '',
+        alt: 'logo',
+        desc: 'Redux Toolkit'
+    },
+    {
+        id: uuidv4(),
+        img: '',
+        alt: 'logo',
+        desc: 'Figma/Inkscape'
+    },
+    {
+        id: uuidv4(),
+        img: '',
+        alt: 'logo',
+        desc: 'MongoDB'
+    },
+    {
+        id: uuidv4(),
+        img: '',
+        alt: 'logo',
+        desc: 'Netlify | Vercel'
+    },
+]

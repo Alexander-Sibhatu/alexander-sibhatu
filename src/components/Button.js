5d52e44b865f55cf91e3315680b2e4eeabb554e0
Alexander Sibhatu <sibhatualexander@gmail.com>
2024-04-05 16:41:13 +0100

    button done

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = () => {
   return (
-    <div className='border-2 hover:border-[#1ABC9C] rounded-md bg-[#1ABC9C] px-4 py-1 my-4 font-4xl font-semibold text-[#FFF8E7] hover:text-[#1ABC9C] hover:bg-[#FFF8E7] drop-shadow-lg cursor-pointer'>CHECK MY RESUME</div>
+    <a href='/Alexanders-CV.pdf' target='_blank' className='border-2 w-content hover:border-[#1ABC9C] hover:translate-x-1 transition transform ease-in-out duration-350 rounded-md bg-[#1ABC9C] px-4 py-1 my-4 font-4xl font-semibold text-[#FFF8E7] hover:text-[#1ABC9C] hover:bg-[#FFF8E7] drop-shadow-lg cursor-pointer'>CHECK MY RESUME</a>
   )
 }
 

a248766289ffbcf91d05e419bb9869997b9e9e39
Alexander Sibhatu <sibhatualexander@gmail.com>
2025-06-20 10:57:50 +0100

    Updated CV

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = () => {
   return (
-    <a href='/Alexander_Sibhatu_CV_24_Oct.pdf' target='_blank' className='border-2 w-content hover:border-[#1ABC9C] hover:translate-x-1 transition transform ease-in-out duration-350 rounded-md bg-[#1ABC9C] px-4 py-1 my-4 font-4xl font-semibold text-[#FFF8E7] hover:text-[#1ABC9C] hover:bg-[#FFF8E7] drop-shadow-lg cursor-pointer'>CHECK MY RESUME</a>
+    <a href='/Alexander-CV-2025-June.pdf' target='_blank' className='border-2 w-content hover:border-[#1ABC9C] hover:translate-x-1 transition transform ease-in-out duration-350 rounded-md bg-[#1ABC9C] px-4 py-1 my-4 font-4xl font-semibold text-[#FFF8E7] hover:text-[#1ABC9C] hover:bg-[#FFF8E7] drop-shadow-lg cursor-pointer'>CHECK MY RESUME</a>
   )
 }
 
